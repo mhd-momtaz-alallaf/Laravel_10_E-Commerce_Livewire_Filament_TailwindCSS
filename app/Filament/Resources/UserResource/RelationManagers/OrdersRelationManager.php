00cf03c6279d4d068bc0f98ba450ba96477481f9
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-04 21:56:42 +0300

    Managing the orders relation of the User Model by "php artisan make:filament-relation-manager UserResource orders id"

---
+++ app/Filament/Resources/UserResource/RelationManagers/OrdersRelationManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrdersRelationManager extends RelationManager // // this OrdersRelationManager class is created by the command "php artisan make:filament-relation-manager UserResource orders id", 'UserResource' is the owner of the relationship, 'orders' is the relation name in the User model, 'id' is a column of the Order model that will be used to identify the orders of the user.
+{
+    protected static string $relationship = 'orders';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('id')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+                Tables\Columns\TextColumn::make('id'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
