8f370d1faae6cb3811e481e6f085c8e46bcd422e
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-04 23:59:37 +0300

    Implementing the Table of the LatestOrders Dashboard Widget

--- app/Filament/Widgets/LatestOrders.php
+++ app/Filament/Widgets/LatestOrders.php
@@ -2,20 +2,63 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Resources\OrderResource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
 
-class LatestOrders extends BaseWidget
+class LatestOrders extends BaseWidget // this Widget is to show the latest 5 orders and it will be used in the dashboard.
 {
+    protected int | string | array $columnSpan = 'full'; // This property allows the table to take the full width of the screen.
+
     public function table(Table $table): Table
     {
         return $table
-            ->query(
-                // ...
-            )
+            ->query(OrderResource::getEloquentQuery()) // Getting the Data from the OrderResource.
+                ->defaultPaginationPageOption(5) // setting the pagination to 5 records per page.
+                ->defaultSort('created_at', 'desc') // sorting by 'created_at' field.   
             ->columns([
-                // ...
+                TextColumn::make('id')
+                    ->label('Order Id')
+                    ->searchable(),
+
+                TextColumn::make('user.name') // getting the name of the user using the 'user' relation in the Order Model.
+                    ->searchable(),
+
+                TextColumn::make('grand_total')
+                    ->money('USD'),
+
+                TextColumn::make('status')
+                    ->badge() // this will make the status showing as badge form.
+                    ->color(fn (string $state):string => match($state){
+                        'new' => 'info', // on the left database field, on the right the wanted filament colors.
+                        'processing'=> 'warning',
+                        'shipped'=> 'success',
+                        'delivered'=> 'success',
+                        'canceled'=> 'danger',
+                    })
+                    ->icon(fn (string $state):string => match($state){
+                        'new' => 'heroicon-m-sparkles', // on the left database field, on the right the wanted icons.
+                        'processing' => 'heroicon-m-arrow-path',
+                        'shipped' => 'heroicon-m-truck',
+                        'delivered' => 'heroicon-m-check-badge',
+                        'canceled' => 'heroicon-m-x-circle',
+                    })
+                    ->sortable(),
+
+                TextColumn::make('payment_method')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('payment_status')
+                    ->badge() // this will make the payment_status showing as badge form.
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->label('Ordering Date')
+                    ->dateTime(),
             ]);
     }
 }
