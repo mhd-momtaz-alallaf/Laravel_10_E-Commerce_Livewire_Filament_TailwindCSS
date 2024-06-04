2c627ba07164ac8918095234bcc8f5887d21a347
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-04 21:20:47 +0300

    Create getTabs() function in the listOrders File to filter the orders list by the order status.

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource;
 use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -22,6 +23,18 @@ class ListOrders extends ListRecords
     {
         return [
             OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array // This function is to show filtering Tabs by the order status.
+    {
+        return [
+            null => Tab::make('All'), // to show all orders.
+            'new' => Tab::make()->query(fn ($query) => $query->where('status','new')), // to show the new orders.
+            'processing' => Tab::make()->query(fn ($query) => $query->where('status','processing')), // to show the processing orders.
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status','shipped')), // to show the shipped orders.
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status','delivered')), // to show the delivered orders.
+            'canceled' => Tab::make()->query(fn ($query) => $query->where('status','canceled')), // to show the canceled orders.
         ];
     }
 }
