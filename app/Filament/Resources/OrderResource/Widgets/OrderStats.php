ef3f55574b62e05e5689ffd899d65232e543e452
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-04 17:33:53 +0300

    Create Average Price Stat widget

--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\OrderResource\Widgets;
 use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class OrderStats extends BaseWidget
 {
@@ -16,6 +17,8 @@ class OrderStats extends BaseWidget
             Stat::make('Processing Orders', Order::query()->where('status','processing')->count()), // This Widget is for getting the processing orders count and show it as a card in the orders list page.
 
             Stat::make('Shipped Orders', Order::query()->where('status','shipped')->count()), // This Widget is for getting the shipped orders count and show it as a card in the orders list page.
+
+            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total'), 'USD')), // This Widget is for getting the average price of all the orders.
         ];
     }
 }
