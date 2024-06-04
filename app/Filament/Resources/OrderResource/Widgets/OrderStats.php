ccc9ca6f7e0dc8a01dc2c1d9e8437f8ce99944fa
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-04 17:21:30 +0300

    Create New Order Stat widget to show the new orders count as a card in the orders list page

--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 
+use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +11,7 @@ class OrderStats extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('New Orders', Order::query()->where('status','new')->count()), // This Widget is for getting the new orders count and show it as a card in the orders list page.
         ];
     }
 }
