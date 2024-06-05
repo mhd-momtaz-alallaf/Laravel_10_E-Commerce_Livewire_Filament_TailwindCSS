9b3e4a3eaf131c58acdbeb6adcdadc1c1655b20b
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-05 11:43:08 +0300

    Configuring the TailwindCSS paths and files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [ // Configure the template paths.
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
   theme: {
     extend: {},
   },
