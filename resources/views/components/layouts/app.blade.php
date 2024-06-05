790a6731e83e2f20858607851dd2130a7704b5c1
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-05 12:17:00 +0300

    Modifying the newly created layout file

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'E-Commerce' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js']) {{-- Including the app.css and app.js --}}
+        @livewireStyles {{-- Including the livewire Styles --}}
     </head>
-    <body>
-        {{ $slot }}
+    <body class="bg-slate-200 dark:bg-slate-700"> {{-- Adding some of tailwindcss styles --}}
+        <main>
+            {{ $slot }}
+        </main>
+        @livewireScripts {{-- Including the livewire Scripts --}}
     </body>
 </html>
