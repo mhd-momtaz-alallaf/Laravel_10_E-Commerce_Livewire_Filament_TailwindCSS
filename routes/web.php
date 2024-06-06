80a8932a65349e279c97054c014d2e485644dea8
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-06 19:22:48 +0300

    Implementing the categories-page.blade.php and Creating the Route for it in the web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomePage::class);
+
+Route::get('/categories', CategoriesPage::class);
