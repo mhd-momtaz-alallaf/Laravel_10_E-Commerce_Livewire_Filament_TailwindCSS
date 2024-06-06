fef37b2a7351f9904ba7d632405b941958f5722d
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-06 19:57:16 +0300

    Implementing the products-page.blade.php and Creating the Route for it in the web.php file

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomePage::class);
 
 Route::get('/categories', CategoriesPage::class);
+
+Route::get('/products', ProductsPage::class);
