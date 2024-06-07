be9c302d626984ab0784436a02756b82034707ce
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-07 23:24:29 +0300

    Naming all the routes in the web.php file

--- routes/web.php
+++ routes/web.php
@@ -27,30 +27,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', LoginPage::class);
+Route::get('/login', LoginPage::class)
+    ->name('login');
 
-Route::get('/register', RegisterPage::class);
+Route::get('/register', RegisterPage::class)
+    ->name('register');
 
-Route::get('/forget-password', ForgetPasswordPage::class);
+Route::get('/forget-password', ForgetPasswordPage::class)
+    ->name('forget-password');
 
-Route::get('/reset-password', ResetPasswordPage::class);
+Route::get('/reset-password', ResetPasswordPage::class)
+    ->name('reset-password');
 
-Route::get('/', HomePage::class);
+Route::get('/', HomePage::class)
+    ->name('home');
 
-Route::get('/categories', CategoriesPage::class);
+Route::get('/categories', CategoriesPage::class)
+    ->name('categories');
 
-Route::get('/products', ProductsPage::class);
+Route::get('/products', ProductsPage::class)
+    ->name('products');
 
-Route::get('/products/{product}', ProductDetailsPage::class);
+Route::get('/products/{product}', ProductDetailsPage::class)
+    ->name('product-details');
 
-Route::get('/cart', CartPage::class);
+Route::get('/cart', CartPage::class)
+    ->name('cart');
 
-Route::get('/checkout', CheckoutPage::class);
+Route::get('/checkout', CheckoutPage::class)
+    ->name('checkout');
 
-Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders', MyOrdersPage::class)
+    ->name('my-orders');
 
-Route::get('/my-orders/{order}', MyOrderDetailsPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailsPage::class)
+    ->name('my-order-details');
 
-Route::get('/success', SuccessPage::class);
+Route::get('/success', SuccessPage::class)
+    ->name('success');
 
-Route::get('/cancel', CancelPage::class);
+Route::get('/cancel', CancelPage::class)
+    ->name('cancel');
