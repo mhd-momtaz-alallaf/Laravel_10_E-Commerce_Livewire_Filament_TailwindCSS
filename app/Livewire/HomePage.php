2dc56577d84f11c90e7a736d6c9abfbad45e01eb
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-08 23:43:12 +0300

    Changing the Title of the Home Page by the Livewire Title attribute

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,8 +2,11 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Home Page | E-Commerce')] // Livewire Attribute for Changing the Default page title.
+    
 class HomePage extends Component
 {
     public function render()
