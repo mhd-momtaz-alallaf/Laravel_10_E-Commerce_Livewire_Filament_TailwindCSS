37669a798c04588504e342e4e5461e00c53d8591
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-09 01:06:11 +0300

    Listing the Brands items dynamically from the database in the home.brands.blade.php via the Brands Component Class

--- app/Livewire/Home/Brands.php
+++ app/Livewire/Home/Brands.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire\Home;
 
+use App\Models\Brand;
 use Livewire\Component;
 
 class Brands extends Component
 {
     public function render()
     {
-        return view('livewire.home.brands');
+        $brands = Brand::where('is_active', 1)->get(); // Getting all active brands form the database.
+
+        return view('livewire.home.brands',[
+            'brands' => $brands // Passing the $brands collection to the view.
+        ]);
     }
 }
