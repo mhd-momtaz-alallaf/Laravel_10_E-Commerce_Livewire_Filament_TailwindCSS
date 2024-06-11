e5cf54f75248e19020ae1e070dffe269a7848e1b
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-11 17:44:00 +0300

    Listing the Categories items dynamically from the database in the home.categories.blade.php via the Categories Component Class

--- app/Livewire/Home/Categories.php
+++ app/Livewire/Home/Categories.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire\Home;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class Categories extends Component
 {
     public function render()
     {
-        return view('livewire.home.categories');
+        $categories = Category::where('is_active', 1)->get(); // Getting all active categories form the database.
+
+        return view('livewire.home.categories', [
+            'categories'=> $categories, // Passing the $categories collection to the view.
+        ]);
     }
 }
