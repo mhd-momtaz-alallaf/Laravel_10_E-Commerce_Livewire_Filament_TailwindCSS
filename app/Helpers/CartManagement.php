2ac7c9e2b96df817b36e49e446dac017abbfcb5b
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-12 20:07:55 +0300

    Create CartManagement Helper Class

---
+++ app/Helpers/CartManagement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+class CartManagement{ // This helper will Provide:
+    // add item to the cart
+
+    // remove item from the cart
+
+    // add cart items to the Cookie
+
+    // clean cart items from the Cookie
+
+    // get all cart items from the Cookie
+
+    // increment items Quantity
+
+    // decrement items Quantity
+
+    // calculate grand total
+}
