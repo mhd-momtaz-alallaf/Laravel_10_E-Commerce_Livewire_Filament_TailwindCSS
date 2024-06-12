f8d6d15be33456927a2f1ab4f572ba10c928a4f2
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-12 18:03:00 +0300

    Modifying the categories-page.blade.php and Adding a Dynamic Link that takes the users to the products page with applied categories filter

--- resources/views/livewire/categories-page.blade.php
+++ resources/views/livewire/categories-page.blade.php
@@ -1,14 +1,18 @@
 <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
     <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 sm:gap-6">
+            {{-- Listing the categories items dynamically from the database, the $categories collection has been passed from the CategoriesPage Component Class --}}
             @foreach ($categories as $category)
-                {{-- Listing the categories items dynamically from the database, the $categories collection has been passed from the CategoriesPage Component Class --}}
-                <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#" wire:key="{{$category->id}}">
+                {{-- The Category Products page Link --}}
+                <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    href="/products?selected_categories[0]={{ $category->id }}" wire:key="{{$category->id}}">
                     <div class="p-4 md:p-5">
                         <div class="flex justify-between items-center">
                             <div class="flex items-center">
-                                <img class="h-[5rem] w-[5rem]" src="{{url('storage', $category->image)}}" alt="{{$category->name}}">
+                                {{-- Category Image Path --}} {{-- . ltrim($category->image, '/') will remove any any leading slashes (/image_path) from the $category->image path, ensuring there are no double slashes in the resulting URL --}}
+                                <img class="h-[5rem] w-[5rem]" src="{{url('storage/' . ltrim($category->image, '/') )}}" alt="{{$category->name}}">
                                 
+                                {{-- Category Name --}}
                                 <div class="ms-3">
                                     <h3 class="group-hover:text-blue-600 text-2xl font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
                                         {{$category->name}}
