76675fd0c01428465bfb3d9b75dde8dab13ea043
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-12 20:23:09 +0300

    Modifying the CartManagement Class and Creating addCartItemsToCookie() and CleanCartItemsFromCookie() functions

--- app/Helpers/CartManagement.php
+++ app/Helpers/CartManagement.php
@@ -2,14 +2,22 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\Cookie;
+
 class CartManagement{ // This helper will Provide:
     // add item to the cart
 
     // remove item from the cart
 
     // add cart items to the Cookie
+    public static function addCartItemsToCookie($cart_items){
+        Cookie::queue('cart_items', json_encode($cart_items), 60 * 24 * 30); // converting the cart_items to json format, cookie name is cart_items, the expiration time will be 30 days.
+    }
 
     // clean cart items from the Cookie
+    public static function CleanCartItemsFromCookie(){
+        Cookie::queue(Cookie::forget('cart_items')); // Forget the Cookie named 'cart_items'.
+    }
 
     // get all cart items from the Cookie
 
