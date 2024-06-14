5c825a7a5cf447cf71f7b203d656bb931fe1e571
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-14 17:55:26 +0300

    Modifying the products-page.blade.php and adding livewire event for temporally changing the 'Add to Cart' Button name just after pressing it

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -148,12 +148,17 @@
 
                                     {{-- Add To Cart Button --}}
                                     <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
-                                        {{-- creating a livewire adding the product to the cart event by the method addToCart() from the component class --}} {{-- we used .prevent to prevent the page from reloading after clicking the 'Add to Cart' button --}}
+                                        {{-- creating a livewire (adding the product to the cart() event and associate it with the method addToCart() in the component class --}} {{-- we used .prevent to prevent the page from reloading after clicking the 'Add to Cart' button --}}
                                         <a wire:click.prevent='addToCart({{ $product->id }})' href="#" class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
                                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                                             </svg>
-                                            <span>Add to Cart</span>
+
+                                            {{-- Button Name will Temporally changing just after the user is pressed the add to cart button, the 'Add to Cart' will be temporally replaced with 'Adding...' as follow: --}}
+                                            {{-- Removing the 'Add to Cart' -Button Name- when the event 'addToCart({{ $product->id }})' is loading --}}
+                                            <span wire:loading.remove wire:target='addToCart({{ $product->id }})'>Add to Cart</span>
+                                            {{-- Adding the 'Adding...' -Button Name- when the event 'addToCart({{ $product->id }})' is loading --}} 
+                                            <span wire:loading wire:target='addToCart({{ $product->id }})'>Adding...</span>
                                         </a>
                                     </div>
                                 </div>
