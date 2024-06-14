e13877b361c5850cfa817ccbe7b93b0dcc1f91cb
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-14 19:04:23 +0300

    Modifying the ProductDetailsPage Component and Handling the 'Minus' and 'Plus' Button to increase and decrease the product quantity

--- app/Livewire/ProductDetailsPage.php
+++ app/Livewire/ProductDetailsPage.php
@@ -10,11 +10,24 @@ use Livewire\Component;
 
 class ProductDetailsPage extends Component
 {
+    public $quantity = 1; // defining a livewire property for showing the quantity in the view.
     public $product; // defining a livewire property for getting the product that passed from the route.
 
     public function mount(Product $product)
     {
         $this->product = $product; // assigning the $product that coming from the route to the component.
+    }
+
+    public function increaseQuantity() // this method is for handling the pressing event on the (Plus Button) in the view and increase the Quantity of the product by 1.
+    {
+        $this->quantity++;
+    }
+
+    public function decreaseQuantity()  // this method is for handling the pressing event on the (Minus Button) in the view and decrease the Quantity of the product by 1.
+    {
+        if ($this->quantity > 1) { // to avoid getting a negative quantity.
+            $this->quantity--;
+        }
     }
 
     public function render()
--- resources/views/livewire/product-details-page.blade.php
+++ resources/views/livewire/product-details-page.blade.php
@@ -60,19 +60,23 @@
                             </p>
                         </div>
 
+                        {{-- Quantity Section --}}
                         <div class="w-32 mb-8 ">
                             <label for="" class="w-full pb-1 text-xl font-semibold text-gray-700 border-b border-blue-300 dark:border-gray-600 dark:text-gray-400">
                                 Quantity
                             </label>
 
                             <div class="relative flex flex-row w-full h-10 mt-6 bg-transparent rounded-lg">
-                                <button class="w-20 h-full text-gray-600 bg-gray-300 rounded-l outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 hover:text-gray-700 dark:bg-gray-900 hover:bg-gray-400">
+                                {{-- Minus Button --}}
+                                <button wire:click="decreaseQuantity" class="w-20 h-full text-gray-600 bg-gray-300 rounded-l outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 hover:text-gray-700 dark:bg-gray-900 hover:bg-gray-400">
                                     <span class="m-auto text-2xl font-thin">-</span>
                                 </button>
 
-                                <input type="number" readonly class="flex items-center w-full font-semibold text-center text-gray-700 placeholder-gray-700 bg-gray-300 outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-900 focus:outline-none text-md hover:text-black" placeholder="1">
+                                {{-- Quantity --}}
+                                <input  type="number" wire:model="quantity" readonly class="flex items-center w-full font-semibold text-center text-gray-700 placeholder-gray-700 bg-gray-300 outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-900 focus:outline-none text-md hover:text-black" placeholder="1">
                                 
-                                <button class="w-20 h-full text-gray-600 bg-gray-300 rounded-r outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 dark:bg-gray-900 hover:text-gray-700 hover:bg-gray-400">
+                                {{-- Plus Button --}}
+                                <button wire:click="increaseQuantity" class="w-20 h-full text-gray-600 bg-gray-300 rounded-r outline-none cursor-pointer dark:hover:bg-gray-700 dark:text-gray-400 dark:bg-gray-900 hover:text-gray-700 hover:bg-gray-400">
                                     <span class="m-auto text-2xl font-thin">+</span>
                                 </button>
                             </div>
