ad8aee0c41216d652b1f1e5c8d397dbadb58f53a
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-14 20:29:56 +0300

    Modifying the CartPage and Listing the 'Cart Items section' and 'Summary section' dynamically from the database

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Cart | E-Commerce')] // Livewire Attribute for Changing the Default page title.
 
 class CartPage extends Component
 {
+    public $cart_items = []; // property for handling the cart items.
+    public $grand_total; // property for handling the grand total.
+
+    public function mount()
+    {
+        $this->cart_items = CartManagement::getCartItemsFromCookie(); // getting the cart items from the getCartItemsFromCookie() function.
+
+        $this->grand_total = CartManagement::calculateCartItemsGrandTotal($this->cart_items); // getting the grand total from the calculateCartItemsGrandTotal() function.
+    }
+
     public function render()
     {
         return view('livewire.cart-page');
--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -7,6 +7,7 @@
 
         <div class="flex flex-col md:flex-row gap-4">
             <div class="md:w-3/4">
+                {{-- Cart Items Secction --}}
                 <div class="bg-white overflow-x-auto rounded-lg shadow-md p-6 mb-4">
                     <table class="w-full">
                         <thead>
@@ -34,48 +35,56 @@
                         </thead>
 
                         <tbody>
-                            <tr>
-                                <td class="py-4">
-                                    <div class="flex items-center">
-                                        <img class="h-16 w-16 mr-4" src="https://via.placeholder.com/150" alt="Product image">
-                                        <span class="font-semibold">Product name</span>
-                                    </div>
-                                </td>
+                            @forelse ($cart_items as $item)
+                                <tr wire:key="{{ $item['product_id'] }}">
+                                    <td class="py-4">
+                                        <div class="flex items-center">
+                                            <img class="h-16 w-16 mr-4" src="{{ url('storage/' . ltrim($item['image'], '/')) }}" alt="{{ $item['name'] }}">
+                                            <span class="font-semibold">{{ $item['name'] }}</span>
+                                        </div>
+                                    </td>
 
-                                <td class="py-4">
-                                    $19.99
-                                </td>
+                                    <td class="py-4">
+                                        {{ Number::currency($item['unit_amount'], 'USD') }}
+                                    </td>
 
-                                <td class="py-4">
-                                    <div class="flex items-center">
-                                        <button class="border rounded-md py-2 px-4 mr-2">
-                                            -
+                                    <td class="py-4">
+                                        <div class="flex items-center">
+                                            <button class="border rounded-md py-2 px-4 mr-2">
+                                                -
+                                            </button>
+
+                                            <span class="text-center w-8">
+                                                {{ $item['quantity'] }}
+                                            </span>
+
+                                            <button class="border rounded-md py-2 px-4 ml-2">
+                                                +
+                                            </button>
+                                        </div>
+                                    </td>
+
+                                    <td class="py-4">
+                                        {{ Number::currency($item['total_amount'], 'USD') }}
+                                    </td>
+
+                                    <td>
+                                        <button class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">
+                                            Remove
                                         </button>
-
-                                        <span class="text-center w-8">1</span>
-
-                                        <button class="border rounded-md py-2 px-4 ml-2">
-                                            +
-                                        </button>
-                                    </div>
-                                </td>
-
-                                <td class="py-4">
-                                    $19.99
-                                </td>
-
-                                <td>
-                                    <button class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">
-                                        Remove
-                                    </button>
-                                </td>
-                            </tr>
-                            <!-- More product rows -->
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="5" class="tesxt-center py-4 text-4xl font-semibold text-slate-500">No Items Available in the Cart!</td>
+                                </tr>
+                            @endforelse
                         </tbody>
                     </table>
                 </div>
             </div>
 
+            {{-- Summary Section --}}
             <div class="md:w-1/4">
                 <div class="bg-white rounded-lg shadow-md p-6">
                     <h2 class="text-lg font-semibold mb-4">
@@ -84,29 +93,40 @@
 
                     <div class="flex justify-between mb-2">
                         <span>Subtotal</span>
-                        <span>$19.99</span>
+                        <span>
+                            {{ Number::currency($grand_total, 'USD') }}
+                        </span>
                     </div>
 
                     <div class="flex justify-between mb-2">
                         <span>Taxes</span>
-                        <span>$1.99</span>
+                        <span>
+                            {{ Number::currency(0, 'USD') }}
+                        </span>
                     </div>
 
                     <div class="flex justify-between mb-2">
                         <span>Shipping</span>
-                        <span>$0.00</span>
+                        <span>
+                            {{ Number::currency(0, 'USD') }}
+                        </span>
                     </div>
 
                     <hr class="my-2">
 
                     <div class="flex justify-between mb-2">
-                        <span class="font-semibold">Total</span>
-                        <span class="font-semibold">$21.98</span>
+                        <span class="font-semibold">Grand Total</span>
+                        <span class="font-semibold">
+                            {{ Number::currency($grand_total, 'USD') }}
+                        </span>
                     </div>
 
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">
-                        Checkout
-                    </button>
+                    {{-- Checkout Button will show only if there is some items in the cart --}}
+                    @if ($cart_items)
+                        <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">
+                            Checkout
+                        </button>
+                    @endif
                 </div>
             </div>
         </div>
