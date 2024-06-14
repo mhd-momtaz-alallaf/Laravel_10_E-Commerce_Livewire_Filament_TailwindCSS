61d109cd787af930f9999f7b0f04c5cbb81fea1f
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-14 17:32:23 +0300

    Modifying the Home.Navbar Component and Listening to addToCart event From productsPage component to show the total cart items in the navbar.blade.php

--- app/Livewire/Partials/Navbar.php
+++ app/Livewire/Partials/Navbar.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire\Partials;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Navbar extends Component
 {
+    public $total_count = 0; // define a $total_count property to show the total cart items in the navbar.blade.php file.
+
+    public function mount()
+    {
+        $this->total_count = count(CartManagement::getCartItemsFromCookie()); // getting the total_count from the getCartItemsFromCookie method, and it will be updated depending on the updateCartCount listener method.
+    }
+
+    // listening to the dispatch method in the (addToCart function) from the ProductsPage Component to get the $total_count of cart items.
+    #[On('update-cart-count')] // 'update-cart-count' is the event name in the dispatch method.
+    public function updateCartCount($total_count) // updating the $total_count value of the cart items, depending on the 'update-cart-count' event.
+    {
+        $this->total_count = $total_count;
+    }
+
     public function render()
     {
         return view('livewire.partials.navbar');
