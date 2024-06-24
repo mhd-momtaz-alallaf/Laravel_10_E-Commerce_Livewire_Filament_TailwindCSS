5ac2a3f763bd7b505d6e2e95f160708e089b1a4f
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-24 23:58:44 +0300

    Modifying the checkout-page.blade.php and showing the Order Summary dynamically from the database

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Checkout | E-Commerce')] // Livewire Attribute for Changing the Default page title.
 
 class CheckoutPage extends Component
 {
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagement::getCartItemsFromCookie(); // getting the cart items from the cookie.
+        $grand_total = CartManagement::calculateCartItemsGrandTotal($cart_items); // getting the grand total of the cart items.
+
+        return view('livewire.checkout-page', [
+            'cart_items' => $cart_items, // passing the attributes to the view.
+            'grand_total' => $grand_total,
+        ]);
     }
 }
--- resources/views/livewire/checkout-page.blade.php
+++ resources/views/livewire/checkout-page.blade.php
@@ -128,6 +128,7 @@
 		</div>
 
 		<div class="md:col-span-12 lg:col-span-4 col-span-12">
+			{{-- Order Summary Section --}}
 			<div class="bg-white rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
 				<div class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
 					ORDER SUMMARY
@@ -139,7 +140,7 @@
 					</span>
 
 					<span>
-						45,000.00
+						{{ Number::currency($grand_total, 'USD') }}
 					</span>
 				</div>
 
@@ -149,7 +150,7 @@
 					</span>
 
 					<span>
-						0.00
+						{{ Number::currency(0, 'USD') }}
 					</span>
 				</div>
 
@@ -159,7 +160,7 @@
 					</span>
 
 					<span>
-						0.00
+						{{ Number::currency(0, 'USD') }}
 					</span>
 				</div>
 
@@ -170,7 +171,7 @@
                         </span>
 
                         <span>
-                            45,000.00
+                            {{ Number::currency($grand_total, 'USD') }}
                         </span>
                     </div>
 				</hr>
@@ -180,80 +181,37 @@
 				Place Order
 			</button>
 
+			{{-- Basket Summary Section --}}
 			<div class="bg-white mt-4 rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
 				<div class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
 					BASKET SUMMARY
 				</div>
 
 				<ul class="divide-y divide-gray-200 dark:divide-gray-700" role="list">
-					<li class="py-3 sm:py-4">
-						<div class="flex items-center">
-							<div class="flex-shrink-0">
-								<img alt="Neil image" class="w-12 h-12 rounded-full" src="https://iplanet.one/cdn/shop/files/iPhone_15_Pro_Max_Blue_Titanium_PDP_Image_Position-1__en-IN_1445x.jpg?v=1695435917">
-								</img>
+					@foreach ($cart_items as $item)
+						<li class="py-3 sm:py-4" wire:key="{{ $item['product_id'] }}">
+							<div class="flex items-center">
+								<div class="flex-shrink-0">
+									<img alt="{{ $item['name'] }}" class="w-12 h-12 rounded-full" src="https://iplanet.one/cdn/shop/files/iPhone_15_Pro_Max_Blue_Titanium_PDP_Image_Position-1__en-IN_1445x.jpg?v=1695435917">
+									</img>
+								</div>
+
+								<div class="flex-1 min-w-0 ms-4">
+									<p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+										Apple iPhone 15 Pro Max
+									</p>
+
+									<p class="text-sm text-gray-500 truncate dark:text-gray-400">
+										Quantity: 1
+									</p>
+								</div>
+
+								<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+									$320
+								</div>
 							</div>
-
-							<div class="flex-1 min-w-0 ms-4">
-								<p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-									Apple iPhone 15 Pro Max
-								</p>
-
-								<p class="text-sm text-gray-500 truncate dark:text-gray-400">
-									Quantity: 1
-								</p>
-							</div>
-
-							<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-								$320
-							</div>
-						</div>
-					</li>
-
-					<li class="py-3 sm:py-4">
-						<div class="flex items-center">
-							<div class="flex-shrink-0">
-								<img alt="Neil image" class="w-12 h-12 rounded-full" src="https://iplanet.one/cdn/shop/files/iPhone_15_Pro_Max_Blue_Titanium_PDP_Image_Position-1__en-IN_1445x.jpg?v=1695435917">
-								</img>
-							</div>
-
-							<div class="flex-1 min-w-0 ms-4">
-								<p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-									Apple iPhone 15 Pro Max
-								</p>
-
-								<p class="text-sm text-gray-500 truncate dark:text-gray-400">
-									Quantity: 1
-								</p>
-							</div>
-
-							<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-								$320
-							</div>
-						</div>
-					</li>
-
-					<li class="py-3 sm:py-4">
-						<div class="flex items-center">
-							<div class="flex-shrink-0">
-								<img alt="Neil image" class="w-12 h-12 rounded-full" src="https://iplanet.one/cdn/shop/files/iPhone_15_Pro_Max_Blue_Titanium_PDP_Image_Position-1__en-IN_1445x.jpg?v=1695435917">
-								</img>
-							</div>
-
-							<div class="flex-1 min-w-0 ms-4">
-								<p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-									Apple iPhone 15 Pro Max
-								</p>
-
-								<p class="text-sm text-gray-500 truncate dark:text-gray-400">
-									Quantity: 1
-								</p>
-							</div>
-
-							<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-								$320
-							</div>
-						</div>
-					</li>
+						</li>
+					@endforeach
 				</ul>
 			</div>
 		</div>
