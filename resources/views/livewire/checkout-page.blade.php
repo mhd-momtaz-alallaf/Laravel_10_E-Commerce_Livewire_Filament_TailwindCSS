c48859e5af4333487043b65ccf8ec3b0c18c577a
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-25 16:52:20 +0300

    Modifying the checkout-page.blade.php and Validating the user information form fields

--- resources/views/livewire/checkout-page.blade.php
+++ resources/views/livewire/checkout-page.blade.php
@@ -4,218 +4,271 @@
 		Checkout
 	</h1>
 
-	<div class="grid grid-cols-12 gap-4">
-		<div class="md:col-span-12 lg:col-span-8 col-span-12">
-			<!-- Card -->
-			<div class="bg-white rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
-				<!-- Shipping Address -->
-				<div class="mb-6">
-					<h2 class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
-						Shipping Address
-					</h2>
+	{{-- Order Information Form --}}
+	<form wire:submit.prevent="placeOrder"> {{-- preventing submiting the form and performing the 'placeOrder' method instead from the component --}}
+		<div class="grid grid-cols-12 gap-4">
+			{{-- User Information Section --}}
+			<div class="md:col-span-12 lg:col-span-8 col-span-12">
+				<!-- Card -->
+				<div class="bg-white rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
+					<!-- Shipping Address -->
+					<div class="mb-6">
+						<h2 class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
+							Shipping Address
+						</h2>
+	
+						<div class="grid grid-cols-2 gap-4">
+							<div>
+								<label class="block text-gray-700 dark:text-white mb-1" for="first_name">
+									First Name
+								</label>
+	
+								<input wire:model="first_name" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none @error('first_name') border-red-500 @enderror" id="first_name" type="text">
+								</input>
 
-					<div class="grid grid-cols-2 gap-4">
-						<div>
-							<label class="block text-gray-700 dark:text-white mb-1" for="first_name">
-								First Name
-							</label>
-
-							<input class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" id="first_name" type="text">
-							</input>
-						</div>
-
-						<div>
-							<label class="block text-gray-700 dark:text-white mb-1" for="last_name">
-								Last Name
-							</label>
-
-							<input class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" id="last_name" type="text">
-							</input>
-						</div>
-					</div>
-
-					<div class="mt-4">
-						<label class="block text-gray-700 dark:text-white mb-1" for="phone">
-							Phone
-						</label>
-
-						<input class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" id="phone" type="text">
-						</input>
-					</div>
-
-					<div class="mt-4">
-						<label class="block text-gray-700 dark:text-white mb-1" for="address">
-							Address
-						</label>
-
-						<input class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" id="address" type="text">
-						</input>
-					</div>
-
-					<div class="mt-4">
-						<label class="block text-gray-700 dark:text-white mb-1" for="city">
-							City
-						</label>
-
-						<input class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" id="city" type="text">
-						</input>
-					</div>
-
-					<div class="grid grid-cols-2 gap-4 mt-4">
-						<div>
-							<label class="block text-gray-700 dark:text-white mb-1" for="state">
-								State
-							</label>
-
-							<input class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" id="state" type="text">
-							</input>
-						</div>
-
-						<div>
-							<label class="block text-gray-700 dark:text-white mb-1" for="zip">
-								ZIP Code
-							</label>
-
-							<input class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" id="zip" type="text">
-							</input>
-						</div>
-					</div>
-				</div>
-
-				<div class="text-lg font-semibold mb-4">
-					Select Payment Method
-				</div>
-
-				<ul class="grid w-full gap-6 md:grid-cols-2">
-					<li>
-						<input class="hidden peer" id="hosting-small" name="hosting" required="" type="radio" value="hosting-small" />
-
-						<label class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700" for="hosting-small">
-							<div class="block">
-								<div class="w-full text-lg font-semibold">
-									Cash on Delivery
-								</div>
+								@error('first_name')
+									<div class="text-red-500 text-sm">
+										{{ $message }}
+									</div>
+								@enderror
 							</div>
+	
+							<div>
+								<label class="block text-gray-700 dark:text-white mb-1" for="last_name">
+									Last Name
+								</label>
+	
+								<input wire:model="last_name" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none @error('last_name') border-red-500 @enderror" id="last_name" type="text">
+								</input>
 
-							<svg aria-hidden="true" class="w-5 h-5 ms-3 rtl:rotate-180" fill="none" viewbox="0 0 14 10" xmlns="http://www.w3.org/2000/svg">
-								<path 
-                                    d="M1 5h12m0 0L9 1m4 4L9 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-								</path>
-							</svg>
-						</label>
-					</li>
-
-					<li>
-						<input class="hidden peer" id="hosting-big" name="hosting" type="radio" value="hosting-big">
-                            <label class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700" for="hosting-big">
-                                <div class="block">
-                                    <div class="w-full text-lg font-semibold">
-                                        Stripe
-                                    </div>
-                                </div>
-
-                                <svg aria-hidden="true" class="w-5 h-5 ms-3 rtl:rotate-180" fill="none" viewbox="0 0 14 10" xmlns="http://www.w3.org/2000/svg">
-                                    <path 
-                                        d="M1 5h12m0 0L9 1m4 4L9 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                                    </path>
-                                </svg>
-                            </label>
-						</input>
-					</li>
-				</ul>
-			</div>
-			<!-- End Card -->
-		</div>
-
-		<div class="md:col-span-12 lg:col-span-4 col-span-12">
-			{{-- Order Summary Section --}}
-			<div class="bg-white rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
-				<div class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
-					ORDER SUMMARY
-				</div>
-
-				<div class="flex justify-between mb-2 font-bold">
-					<span>
-						Subtotal
-					</span>
-
-					<span>
-						{{ Number::currency($grand_total, 'USD') }}
-					</span>
-				</div>
-
-				<div class="flex justify-between mb-2 font-bold">
-					<span>
-						Taxes
-					</span>
-
-					<span>
-						{{ Number::currency(0, 'USD') }}
-					</span>
-				</div>
-
-				<div class="flex justify-between mb-2 font-bold">
-					<span>
-						Shipping Cost
-					</span>
-
-					<span>
-						{{ Number::currency(0, 'USD') }}
-					</span>
-				</div>
-
-				<hr class="bg-slate-400 my-4 h-1 rounded">
-                    <div class="flex justify-between mb-2 font-bold">
-                        <span>
-                            Grand Total
-                        </span>
-
-                        <span>
-                            {{ Number::currency($grand_total, 'USD') }}
-                        </span>
-                    </div>
-				</hr>
-			</div>
-
-			<button class="bg-green-500 mt-4 w-full p-3 rounded-lg text-lg text-white hover:bg-green-600">
-				Place Order
-			</button>
-
-			{{-- Basket Summary Section --}}
-			<div class="bg-white mt-4 rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
-				<div class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
-					BASKET SUMMARY
-				</div>
-
-				{{-- Basket Items --}}
-				<ul class="divide-y divide-gray-200 dark:divide-gray-700" role="list">
-					@foreach ($cart_items as $item)
-						<li class="py-3 sm:py-4" wire:key="{{ $item['product_id'] }}">
-							<div class="flex items-center">
-								<div class="flex-shrink-0">
-									<img alt="{{ $item['name'] }}" class="w-12 h-12 rounded-full" 
-										src="{{ url('storage/' . ltrim($item['image'], '/')) }}">
-									</img>
-								</div>
-
-								<div class="flex-1 min-w-0 ms-4">
-									<p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-										{{ $item['name'] }}
-									</p>
-
-									<p class="text-sm text-gray-500 truncate dark:text-gray-400">
-										Quantity: {{ $item['quantity'] }}
-									</p>
-								</div>
-
-								<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-									{{ Number::currency($item['total_amount'], 'USD') }} 
-								</div>
+								@error('last_name')
+									<div class="text-red-500 text-sm">
+										{{ $message }}
+									</div>
+								@enderror
 							</div>
+						</div>
+	
+						<div class="mt-4">
+							<label class="block text-gray-700 dark:text-white mb-1" for="phone">
+								Phone
+							</label>
+	
+							<input wire:model="phone" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none @error('phone') border-red-500 @enderror" id="phone" type="text">
+							</input>
+
+							@error('phone')
+								<div class="text-red-500 text-sm">
+									{{ $message }}
+								</div>
+							@enderror
+						</div>
+	
+						<div class="mt-4">
+							<label class="block text-gray-700 dark:text-white mb-1" for="address">
+								Address
+							</label>
+	
+							<input wire:model="street_address" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none @error('street_address') border-red-500 @enderror" id="street_address" type="text">
+							</input>
+
+							@error('street_address')
+								<div class="text-red-500 text-sm">
+									{{ $message }}
+								</div>
+							@enderror
+						</div>
+	
+						<div class="mt-4">
+							<label class="block text-gray-700 dark:text-white mb-1" for="city">
+								City
+							</label>
+	
+							<input wire:model="city" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none @error('city') border-red-500 @enderror" id="city" type="text">
+							</input>
+
+							@error('city')
+								<div class="text-red-500 text-sm">
+									{{ $message }}
+								</div>
+							@enderror
+						</div>
+	
+						<div class="grid grid-cols-2 gap-4 mt-4">
+							<div>
+								<label class="block text-gray-700 dark:text-white mb-1" for="state">
+									State
+								</label>
+	
+								<input wire:model="state" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none @error('state') border-red-500 @enderror" id="state" type="text">
+								</input>
+
+								@error('state')
+									<div class="text-red-500 text-sm">
+										{{ $message }}
+									</div>
+								@enderror
+							</div>
+	
+							<div>
+								<label class="block text-gray-700 dark:text-white mb-1" for="zip">
+									ZIP Code
+								</label>
+	
+								<input wire:model="zip_code" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none @error('zip_code') border-red-500 @enderror" id="zip_code" type="text">
+								</input>
+
+								@error('zip_code')
+									<div class="text-red-500 text-sm">
+										{{ $message }}
+									</div>
+								@enderror
+							</div>
+						</div>
+					</div>
+	
+					<div class="text-lg font-semibold mb-4">
+						Select Payment Method
+					</div>
+	
+					<ul class="grid w-full gap-6 md:grid-cols-2">
+						<li>
+							<input wire:model="payment_method" value="cod" class="hidden peer" id="cod_payment_method" required="" type="radio" />
+
+							<label class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 @error('payment_method') border-red-500 @enderror rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700" for="cod_payment_method">
+								<div class="block">
+									<div class="w-full text-lg font-semibold">
+										Cash on Delivery
+									</div>
+								</div>
+	
+								<svg aria-hidden="true" class="w-5 h-5 ms-3 rtl:rotate-180" fill="none" viewbox="0 0 14 10" xmlns="http://www.w3.org/2000/svg">
+									<path 
+										d="M1 5h12m0 0L9 1m4 4L9 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+									</path>
+								</svg>
+							</label>
 						</li>
-					@endforeach
-				</ul>
+	
+						<li>
+							<input wire:model="payment_method" value="stripe" class="hidden peer" id="stripe_payment_method" type="radio"/>
+
+							<label class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 @error('payment_method') border-red-500 @enderror rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700" for="stripe_payment_method">
+								<div class="block">
+									<div class="w-full text-lg font-semibold">
+										Stripe
+									</div>
+								</div>
+		
+								<svg aria-hidden="true" class="w-5 h-5 ms-3 rtl:rotate-180" fill="none" viewbox="0 0 14 10" xmlns="http://www.w3.org/2000/svg">
+									<path 
+										d="M1 5h12m0 0L9 1m4 4L9 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+									</path>
+								</svg>
+							</label>
+						</li>
+					</ul>
+					@error('payment_method')
+						<div class="text-red-500 text-sm">
+							{{ $message }}
+						</div>
+					@enderror
+				</div>
+				<!-- End Card -->
+			</div>
+
+			{{-- Summary Section --}}
+			<div class="md:col-span-12 lg:col-span-4 col-span-12">
+				{{-- Order Summary Section --}}
+				<div class="bg-white rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
+					<div class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
+						ORDER SUMMARY
+					</div>
+
+					<div class="flex justify-between mb-2 font-bold">
+						<span>
+							Subtotal
+						</span>
+
+						<span>
+							{{ Number::currency($grand_total, 'USD') }}
+						</span>
+					</div>
+
+					<div class="flex justify-between mb-2 font-bold">
+						<span>
+							Taxes
+						</span>
+
+						<span>
+							{{ Number::currency(0, 'USD') }}
+						</span>
+					</div>
+
+					<div class="flex justify-between mb-2 font-bold">
+						<span>
+							Shipping Cost
+						</span>
+
+						<span>
+							{{ Number::currency(0, 'USD') }}
+						</span>
+					</div>
+
+					<hr class="bg-slate-400 my-4 h-1 rounded">
+						<div class="flex justify-between mb-2 font-bold">
+							<span>
+								Grand Total
+							</span>
+
+							<span>
+								{{ Number::currency($grand_total, 'USD') }}
+							</span>
+						</div>
+					</hr>
+				</div>
+
+				{{-- Place Order Button --}}
+				<button type="submit" class="bg-green-500 mt-4 w-full p-3 rounded-lg text-lg text-white hover:bg-green-600">
+					Place Order
+				</button>
+
+				{{-- Basket Summary Section --}}
+				<div class="bg-white mt-4 rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
+					<div class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
+						BASKET SUMMARY
+					</div>
+
+					{{-- Basket Items --}}
+					<ul class="divide-y divide-gray-200 dark:divide-gray-700" role="list">
+						@foreach ($cart_items as $item)
+							<li class="py-3 sm:py-4" wire:key="{{ $item['product_id'] }}">
+								<div class="flex items-center">
+									<div class="flex-shrink-0">
+										<img alt="{{ $item['name'] }}" class="w-12 h-12 rounded-full" 
+											src="{{ url('storage/' . ltrim($item['image'], '/')) }}">
+										</img>
+									</div>
+
+									<div class="flex-1 min-w-0 ms-4">
+										<p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+											{{ $item['name'] }}
+										</p>
+
+										<p class="text-sm text-gray-500 truncate dark:text-gray-400">
+											Quantity: {{ $item['quantity'] }}
+										</p>
+									</div>
+
+									<div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+										{{ Number::currency($item['total_amount'], 'USD') }} 
+									</div>
+								</div>
+							</li>
+						@endforeach
+					</ul>
+				</div>
 			</div>
 		</div>
-	</div>
+	</form>
 </div>
