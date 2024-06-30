218bbddd989b8bf79ddec1b2e3498d0ce85e8f4b
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-30 19:34:35 +0300

    Modifying MyOrdersPage and Passing the user orders to the view file

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
+
+#[Title('My Orders | E-Commerce')] // Livewire Attribute for Changing the Default page title.
 
 class MyOrdersPage extends Component
 {
+    // using WithPagination Trait to have be able to use ->paginate() in the queries inside the livewire component.
+    use WithPagination; 
+
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $my_orders = Order::where('user_id', auth()->user()->id)->latest()->paginate(2);
+
+        return view('livewire.my-orders-page',[
+            'orders' => $my_orders,
+        ]);
     }
 }
