97f75204b5cf37d23e39dd877435254f030a752f
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-06-30 17:49:12 +0300

    Modifying the SuccessPage and Passing the $user_latest_order to the view file

--- app/Livewire/SuccessPage.php
+++ app/Livewire/SuccessPage.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Success | E-Commerce')] // Livewire Attribute for Changing the Default page title.
 
 class SuccessPage extends Component
 {
     public function render()
     {
-        return view('livewire.success-page');
+        // getting the latest order of the current user.
+        $user_latest_order = Order::with('address')->where('user_id', auth()->user()->id)->latest()->first();
+        
+        return view('livewire.success-page',[
+            'order' => $user_latest_order,
+        ]);
     }
 }
