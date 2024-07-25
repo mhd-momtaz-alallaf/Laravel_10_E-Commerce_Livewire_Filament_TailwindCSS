e90d2f48362bf28ba6643f4901e8ac2b9f11a644
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-25 22:02:20 +0300

    Modifying the route of get started button in the get-started.blade.php

--- resources/views/components/home/get-started.blade.php
+++ resources/views/components/home/get-started.blade.php
@@ -9,22 +9,23 @@
                 <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">
                     Purchase wide varities of electronics products like Smartphones, Laptops, Smartwatches, Television and many more.
                 </p>
-        
+
                 <!-- Buttons -->
                 <div class="mt-7 grid gap-3 w-full sm:inline-flex">
-                    <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/register">
+                    <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        href="{{ route('products') }}">
                         Get started
                         <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="m9 18 6-6-6-6" />
                         </svg>
                     </a>
 
-                    <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/contact">
+                    <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
                         Contact sales team
                     </a>
                 </div>
                 <!-- End Buttons -->
-        
+
                 <!-- Reviews -->
                 <div class="mt-6 lg:mt-10 grid grid-cols-2 gap-x-5">
                     <!-- Review 1 -->
@@ -50,11 +51,11 @@
                                 <path d="M27.0352 1.6307L33.9181 16.3633C34.2173 16.6768 34.5166 16.9903 34.8158 16.9903L50.0779 19.1845C50.9757 19.1845 51.275 20.4383 50.6764 21.0652L39.604 32.3498C39.3047 32.6632 39.3047 32.9767 39.3047 33.2901L41.998 49.2766C42.2973 50.217 41.1002 50.8439 40.5017 50.5304L26.4367 43.3208C26.1375 43.3208 25.8382 43.3208 25.539 43.3208L11.7732 50.8439C10.8754 51.1573 9.97763 50.5304 10.2769 49.59L12.9702 33.6036C12.9702 33.2901 12.9702 32.9767 12.671 32.6632L1.29923 21.0652C0.700724 20.4383 0.999979 19.4979 1.89775 19.4979L17.1598 17.3037C17.459 17.3037 17.7583 16.9903 18.0575 16.6768L24.9404 1.6307C25.539 0.69032 26.736 0.69032 27.0352 1.6307Z" fill="currentColor" />
                             </svg>
                         </div>
-            
+
                         <p class="mt-3 text-sm text-gray-800 dark:text-gray-200">
                             <span class="font-bold">4.6</span> /5 - from 12k reviews
                         </p>
-            
+
                         <div class="mt-5">
                             <!-- Star -->
                             <svg class="h-auto w-16 text-gray-800 dark:text-white" width="80" height="27" viewBox="0 0 80 27" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -69,7 +70,7 @@
                         </div>
                     </div>
                     <!-- End Review 1 -->
-        
+
                     <!-- Review 2 -->
                     <div class="py-5">
                         <div class="flex space-x-1">
@@ -94,11 +95,11 @@
                                 <path d="M0.313299 20.0305C-0.288914 19.3946 0.0122427 18.1228 0.915411 18.1228L16.2694 15.8972C16.5704 15.8972 16.8715 15.8972 17.1725 15.2613L24.0968 0.317944C24.3979 0 24.6989 0 25 0V42.6046C25 42.6046 24.6989 42.6046 24.3979 42.6046L10.2482 49.9173C9.64609 50.2352 8.44187 49.5994 8.74292 48.6455L11.4524 32.4303C11.4524 32.1124 11.4524 31.7944 11.1514 31.4765L0.313299 20.0305Z" fill="currentColor" />
                             </svg>
                         </div>
-            
+
                         <p class="mt-3 text-sm text-gray-800 dark:text-gray-200">
                             <span class="font-bold">4.8</span> /5 - from 5k reviews
                         </p>
-            
+
                         <div class="mt-5">
                             <!-- Star -->
                             <svg class="h-auto w-16 text-gray-800 dark:text-white" width="110" height="28" viewBox="0 0 110 28" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -117,11 +118,11 @@
                 <!-- End Reviews -->
             </div>
             <!-- End Col -->
-    
+
             <div class="relative ms-4">
                 <img class="w-full rounded-md" src="https://static.vecteezy.com/system/resources/previews/011/993/278/non_2x/3d-render-online-shopping-bag-using-credit-card-or-cash-for-future-use-credit-card-money-financial-security-on-mobile-3d-application-3d-shop-purchase-basket-retail-store-on-e-commerce-free-png.png" alt="Image Description">
                 <div class="absolute inset-0 -z-[1] bg-gradient-to-tr from-gray-200 via-white/0 to-white/0 w-full h-full rounded-md mt-4 -mb-4 me-4 -ms-4 lg:mt-6 lg:-mb-6 lg:me-6 lg:-ms-6 dark:from-slate-800 dark:via-slate-900/0 dark:to-slate-900/0"></div>
-        
+
                 <!-- SVG-->
                 <div class="absolute bottom-0 start-0">
                     <svg class="w-2/3 ms-auto h-auto text-white dark:text-slate-900" width="630" height="451" viewBox="0 0 630 451" fill="none" xmlns="http://www.w3.org/2000/svg">
